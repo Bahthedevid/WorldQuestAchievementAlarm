060715b24823214d542f52d08e5e04c219aada9a
Michal Kroupa <michalkrou@gmail.com>
2022-05-13 18:58:31 +0200

    removed obsolete code, added slash commands

--- WorldQuestAchievementAlarm.lua
+++ WorldQuestAchievementAlarm.lua
@@ -1,10 +1,10 @@
 local WQAA = LibStub("AceAddon-3.0"):NewAddon("WorldQuestAchievementAlarm", "AceConsole-3.0")
-local AceGUI = LibStub("AceGUI-3.0")
 
 WQAA.questIds = {59717, 52798, 51957, 51983, 51173, 50665, 50717, 50899, 51977, 51947, 51974, 51976, 51978, 54415, 54689, 63972, 63836, 63837, 63945, 63846, 63773, 64016, 63989, 64017, 59705}
 WQAA.mapIds = {1536, 1533, 1565, 1525, 863, 864, 942, 896, 1961, 1960}
 
 function WQAA:CheckAvailableWQs()
+	-- Check if ther are any tracked WQs available
 	local availableQuests = {}
 	for _, mapId in ipairs(WQAA.mapIds) do
 		local mapInfo = C_Map.GetMapInfo(mapId)
@@ -14,7 +14,7 @@ function WQAA:CheckAvailableWQs()
 		for _, info in pairs(mapQuests) do
 			if WQAA:isInTable(WQAA.questIds, info.questId) == true then
 				local questTitle, factionID, capped, displayAsObjective = C_TaskQuest.GetQuestInfoByQuestID(info.questId)
-				table.insert(availableMapQuests, questTitle)
+				table.insert(availableMapQuests, {questTitle, info.questId})
 			end
 		end
 		
@@ -23,6 +23,7 @@ function WQAA:CheckAvailableWQs()
 		end
 	end
 	
+	-- Show frame with available WQs or just print that there are none in chat window
 	if next(availableQuests) ~= nil then
 		local frame = AceGUI:Create("Frame")
 		frame:SetTitle("WQAA's WQ list")
@@ -31,7 +32,6 @@ function WQAA:CheckAvailableWQs()
 		frame:SetHeight(200)
 
 		for mapName, quests in pairs(availableQuests) do
-			WQAA:Print(quests[1])
 			local h = AceGUI:Create("Heading")
 			h:SetText(mapName)
 			h:SetWidth(200)
@@ -39,9 +39,10 @@ function WQAA:CheckAvailableWQs()
 
 			for _, quest in pairs(quests) do
 				local l = AceGUI:Create("InteractiveLabel")
-				l:SetText(quest)
+				l:SetText(quest[1] .. " " .. quest[2])
 				l:SetWidth(200)
 				l:SetColor(0, 255, 0)
+				l:SetCallback("OnClick", function() C_QuestLog.AddWorldQuestWatch(quest[2]) end)
 				frame:AddChild(l)
 			end
 		end
@@ -50,12 +51,20 @@ function WQAA:CheckAvailableWQs()
 	end
 end
 
-function WQAA:OnEnable()
-	WQAA:Print("Enabled")
+function WQAA:SlashCommand()
 	WQAA:CheckAvailableWQs()
-	--	WQAA:ToastFakeAchievement()
 end
 
+function WQAA:OnInitialize()
+	self:RegisterChatCommand("wqaa", "SlashCommand")
+end
+
+function WQAA:OnEnable()
+	WQAA:Print("Enabled")
+--	WQAA:CheckAvailableWQs()
+end
+
+-- Check if value is in hash table
 function WQAA:isInTable(tab, val)
     for index, value in ipairs(tab) do
         if value == val then
@@ -65,35 +74,3 @@ function WQAA:isInTable(tab, val)
 
     return false
 end 
-
-function WQAA:ToastFakeAchievement()
-	if (Kiosk.IsEnabled()) then
-	  return;
-	end
-	if ( not AchievementFrame ) then
-	  AchievementFrame_LoadUI();
-	end
-
-	WQAA.AlertSystem = AlertFrame:AddQueuedAlertFrameSubSystem("WQAAAlertFrameTemplate", WQAAAlertFrame_SetUp, 4, math.huge)
-	  
-	WQAA.AlertSystem:AddAlert()
-  
-  end
-
-  
-local function WQAAAlertFrame_SetUp(frame)
-	-- An alert flagged as alreadyEarned has more space for the text to display since there's no shield+points icon.
-	local ret = AchievementAlertFrame_SetUp(frame, 5208, false)
-	frame.Name:SetText("test title")
-	frame.Unlocked:SetText("toptext")
-	frame.onClick = function()
-		Calendar_LoadUI()
-		if (Calendar_Show) then  Calendar_Show();  end
-	end
-	frame.delay = 0
-
-	frame.Icon.Texture:SetTexCoord(0, 0, 0, 1, 1, 0, 1, 1)
-	frame.Background:SetTexture("Interface\\AchievementFrame\\UI-Achievement-Alert-Background")
-	--frame.OldAchievement:SetTexture("Interface\\AchievementFrame\\UI-Achievement-Borders")
-	frame.OldAchievement:Hide()
-end
