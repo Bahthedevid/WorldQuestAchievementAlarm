54351fdba62bc15151b05cf5ea42ebd0a3e54bf6
Frederik <nielsen2204@gmail.com>
2022-05-16 17:49:11 +0200

    More dialog work

--- WorldQuestAchievementAlarm.lua
+++ WorldQuestAchievementAlarm.lua
@@ -62,10 +62,10 @@ local function setupStandardFrame(frame, strata, xSize, ySize, point, background
 end
 
 local function AddMainUIFrame()
-	-- Main UI frame
-	setupStandardFrame(dialogFrame, "HIGH", 800, 500, "CENTER", 0.75)
+	-- Main frame
+	setupStandardFrame(dialogFrame, "MEDIUM", 300, 500, "CENTER", 0.75)
 
-	-- Main UI frame title:
+	-- Main frame title:
 	local title = dialogFrame:CreateFontString(dialogFrame, "OVERLAY", "GameFontNormal")
 	title:SetPoint("TOP", 0, -4)
 	title:SetText("World Quest Achievement Alarm")
@@ -92,11 +92,11 @@ local function OpenAddWindow()
 end
 
 local function AddNewTrackedObjectDialog()
-	-- Add new tracked quest dialog
-	setupStandardFrame(addTrackerDialog, "DIALOG", 400, 200, "CENTER", 1)
+	-- Add-new dialog
+	setupStandardFrame(addTrackerDialog, "HIGH", 400, 200, "CENTER", 1)
 	addTrackerDialog:Hide()
 
-	-- Add new tracked WQ dialog title:
+	-- Add-new dialog title:
 	local title = addTrackerDialog:CreateFontString(addTrackerDialog, "OVERLAY", "GameFontNormal")
 	title:SetPoint("TOP", 0, -4)
 	title:SetText("Add new tracked world quest")
@@ -107,25 +107,31 @@ local function AddNewTrackedObjectDialog()
 
 	-- Input fields
 	local achievInput = CreateFrame("EditBox", "achievInput", addTrackerDialog, "ChatFrameEditBoxTemplate")
+	achievInput:SetFrameStrata("DIALOG")
 	achievInput:SetPoint("LEFT", addTrackerDialog, 20, 0)
 	achievInput:SetPoint("RIGHT", addTrackerDialog, -20, 0)
+	achievInput:SetHeight(24)
 	achievInput:SetMultiLine(false)
 	achievInput:SetAutoFocus(true)
 	achievInput:SetScript("OnEscapePressed", function() addTrackerDialog:Hide() end)
+
+	-- Input field labels
+	local text = addTrackerDialog:CreateFontString(addTrackerDialog, "OVERLAY", "GameTooltipText")
+	text:SetPoint("LEFT", achievInput, 0, 10)
+	text:SetText("Achievement name:")
 
 	-- Close button
 	addTrackerDialog.CloseButton = CreateFrame("Button", nil, addTrackerDialog, "UIPanelCloseButton")
 	addTrackerDialog.CloseButton:SetPoint("TOPRIGHT", addTrackerDialog, "TOPRIGHT", 4, 3);
 	addTrackerDialog.CloseButton:SetScript("OnClick", OnCloseButtonPressed);
 	
-	-- TrackNew button
-	local TrackNew = CreateFrame("Button", "TrackNew", dialogFrame, "UIPanelButtonTemplate")
-	TrackNew:SetWidth(50)
-	TrackNew:SetHeight(25)
-	TrackNew:SetPoint("BOTTOMLEFT", 5, 5)
-	TrackNew:SetText("Add")
-	TrackNew:RegisterForClicks("AnyUp")
-	TrackNew:SetScript("OnClick", OpenAddWindow)
+	-- Add-new button
+	local AddNew = CreateFrame("Button", "AddNew", dialogFrame, "UIPanelButtonTemplate")
+	AddNew:SetPoint("TOPLEFT", dialogFrame, "BOTTOMLEFT", 10, 30)
+	AddNew:SetPoint("BOTTOMRIGHT", dialogFrame, "BOTTOMRIGHT", -10, 10)
+	AddNew:SetText("Add")
+	AddNew:RegisterForClicks("AnyUp")
+	AddNew:SetScript("OnClick", OpenAddWindow)
 end
 
 local function Close()
