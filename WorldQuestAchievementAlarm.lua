aa0fd8f1837b596716d773863ba9258c3feba834
Frederik <nielsen2204@gmail.com>
2022-05-17 14:09:38 +0200

    Layout of list items

--- WorldQuestAchievementAlarm.lua
+++ WorldQuestAchievementAlarm.lua
@@ -1,33 +1,56 @@
 local WorldQuestAchievementAlarm = LibStub("AceAddon-3.0"):NewAddon("WorldQuestAchievementAlarm", "AceConsole-3.0")
 local WQAA = WorldQuestAchievementAlarm
 
-local dialogFrame = CreateFrame("Frame", "MainWindow", UIParent, "BackdropTemplate")
-local addTrackerDialog = CreateFrame("Frame", "AddTrackerDialog", dialogFrame, "BackdropTemplate")
+local mainWindow = CreateFrame("Frame", "MainWindow", UIParent, "BackdropTemplate")
+local mainWindowTitle = mainWindow:CreateFontString(mainWindow, "OVERLAY", "GameFontNormal")
+local scrollFrame = CreateFrame("ScrollFrame", "WQAAScrollFrame", mainWindow, "UIPanelScrollFrameTemplate")
+local addNewWindow = CreateFrame("Frame", "AddTrackerDialog", mainWindow, "BackdropTemplate")
+local mainWindowAddNewButton = CreateFrame("Button", "AddNew", mainWindow, "UIPanelButtonTemplate")
+local addNewWindowAddButton = CreateFrame("Button", "DialogAdd", addNewWindow, "UIPanelButtonTemplate")
 
-local function AddToFrame(QuestTitle)
-	local text = dialogFrame:CreateFontString(dialogFrame, "OVERLAY", "GameTooltipText")
-	text:SetPoint("CENTER", 0, 0)
-	text:SetText(QuestTitle)
+
+local function CreateListElement()
+	local firstListEntry = scrollFrame:CreateFontString(scrollFrame, "OVERLAY", "GameFontNormal")
+	firstListEntry:SetPoint("LEFT", scrollFrame, "LEFT")
+	firstListEntry:SetPoint("RIGHT", scrollFrame, "RIGHT")
+	firstListEntry:SetJustifyH("LEFT")
+	return firstListEntry
 end
 
-local function SeeIfWorldQuestIsActive(questID)
-	local QuestTitle, _, _, _ = C_TaskQuest.GetQuestInfoByQuestID(questID)
-	local isActive = C_TaskQuest.IsActive(questID)
-	if (isActive) then
-		-- Add world quest to frame
-		AddToFrame(QuestTitle)
-		WQAA:Print(QuestTitle .. " is currently active!")
+local function BuildListOfActiveWorldQuests(questList)
+	local firstListElement = CreateListElement()
+	firstListElement:SetPoint("TOPLEFT", scrollFrame, "TOPLEFT")
+	local previousFrame = firstListElement
+	for key, value in pairs(questList) do
+		WQAA:Print("MAP: " .. key .. " " .. value)
+		listEntry = CreateListElement()
+		listEntry:SetText(key)
+		listEntry:SetPoint("TOP", previousFrame, "BOTTOM", 0, -8)
+		listEntry:SetPoint("LEFT", scrollFrame, "LEFT")
+		tabbedListEntry = scrollFrame:CreateFontString(scrollFrame, "OVERLAY", "GameFontNormal")
+		tabbedListEntry:SetText(value)
+		tabbedListEntry:SetPoint("TOPLEFT", listEntry, "BOTTOMLEFT", 15, -4)
+		previousFrame = tabbedListEntry
 	end
+end
+
+local function WorldQuestIsActive(questID)
+	return C_TaskQuest.IsActive(questID)
 end
 
 local function ScanAchievements()
+	local activeUnfinishedQuests = {}
 	for key, value in pairs(WQAAQuestData) do
-		local _, _, _, Completed, _, _, _, _, _, _, _, _ = GetAchievementInfo(key)
+		local _, name, _, completed, _, _, _, _, _, _, _, _ = GetAchievementInfo(key)
+		local questTitle, _, _, _ = C_TaskQuest.GetQuestInfoByQuestID(value)
 		WQAA:Print("VALUES: " .. key .. " " .. value)
-		if (Completed == false) then
-			SeeIfWorldQuestIsActive(value)
+		if (completed == false) then
+			if (WorldQuestIsActive(value)) then
+				activeUnfinishedQuests[name] = questTitle
+			end
 		end
 	end
+	BuildListOfActiveWorldQuests(activeUnfinishedQuests)
 end
 
 -----------------------------------------
@@ -61,85 +84,94 @@ local function setupStandardFrame(frame, strata, xSize, ySize, point, background
 	frame:SetBackdropBorderColor(0, 0, 0)
 end
 
-local function AddMainUIFrame()
-	-- Main frame
-	setupStandardFrame(dialogFrame, "MEDIUM", 300, 500, "CENTER", 0.75)
-
-	-- Main frame title:
-	local title = dialogFrame:CreateFontString(dialogFrame, "OVERLAY", "GameFontNormal")
-	title:SetPoint("TOP", 0, -4)
-	title:SetText("World Quest Achievement Alarm")
-	-- Make it easy to access:
-	dialogFrame.Title = title
-
-	MarkWindowForMovement(dialogFrame)
-
-	-- Scrollframe
-	-- local scrollFrame = CreateFrame("ScrollFrame", "WQAAScrollFrame", dialogFrame, "UIPanelScrollFrameTemplate")
-	-- scrollFrame:SetPoint("LEFT", 16, 0)
-	-- scrollFrame:SetPoint("RIGHT", -32, 0)
-	-- scrollFrame:SetPoint("TOP", 0, -16)
-	-- scrollFrame:SetPoint("BOTTOM", dialogFrame, "TOP", 0, 0)
-	
-	-- Close button
-	dialogFrame.CloseButton = CreateFrame("Button", nil, dialogFrame, "UIPanelCloseButton")
-	dialogFrame.CloseButton:SetPoint("TOPRIGHT", dialogFrame, "TOPRIGHT", 4, 3);
-	dialogFrame.CloseButton:SetScript("OnClick", OnCloseButtonPressed);
+local function OpenAddWindow()
+	addNewWindow:Show()
 end
 
-local function OpenAddWindow()
-	addTrackerDialog:Show()
+local function AddMainUIFrame()
+	-- Main frame
+	setupStandardFrame(mainWindow, "MEDIUM", 300, 500, "CENTER", 0.75)
+
+	-- Main frame title:
+	mainWindowTitle:SetPoint("TOP", mainWindow, "TOP", 0, -6)
+	mainWindowTitle:SetText("World Quest Achievement Alarm")
+	mainWindowTitle:SetFont("Fonts\\FRIZQT__.TTF", 15, "OUTLINE")
+	-- Make it easy to access:
+	mainWindow.Title = mainWindowTitle
+
+	MarkWindowForMovement(mainWindow)
+
+	-- Scrollframe
+	scrollFrame:SetPoint("TOPLEFT", mainWindowTitle, "BOTTOMLEFT", -10, -10)
+	scrollFrame:SetPoint("TOPRIGHT", mainWindowTitle, "BOTTOMRIGHT", -5, -10)
+	scrollFrame:SetPoint("BOTTOMLEFT", mainWindowAddNewButton, "TOPLEFT", 0, 10)
+	scrollFrame:SetPoint("BOTTOMRIGHT", mainWindowAddNewButton, "TOPRIGHT", 0, 10)
+
+	-- Close button
+	mainWindow.CloseButton = CreateFrame("Button", nil, mainWindow, "UIPanelButtonTemplate")
+	mainWindow.CloseButton:SetPoint("BOTTOMLEFT", mainWindow, "BOTTOM", 10, 10);
+	mainWindow.CloseButton:SetPoint("TOPRIGHT", mainWindow, "BOTTOMRIGHT", -10, 30);
+	mainWindow.CloseButton:SetScript("OnClick", OnCloseButtonPressed);
+	mainWindow.CloseButton:SetText("Close")
+
+	-- Add-new button
+	mainWindowAddNewButton:SetPoint("TOPLEFT", mainWindow, "BOTTOMLEFT", 10, 30)
+	mainWindowAddNewButton:SetPoint("BOTTOMRIGHT", mainWindow, "BOTTOM", -10, 10)
+	mainWindowAddNewButton:SetText("Add")
+	mainWindowAddNewButton:RegisterForClicks("AnyUp")
+	mainWindowAddNewButton:SetScript("OnClick", OpenAddWindow)
 end
 
 local function AddNewTrackedObjectDialog()
 	-- Add-new dialog
-	setupStandardFrame(addTrackerDialog, "HIGH", 400, 200, "CENTER", 1)
-	addTrackerDialog:Hide()
+	setupStandardFrame(addNewWindow, "HIGH", 400, 200, "CENTER", 1)
+	addNewWindow:Hide()
 
 	-- Add-new dialog title:
-	local title = addTrackerDialog:CreateFontString(addTrackerDialog, "OVERLAY", "GameFontNormal")
+	local title = addNewWindow:CreateFontString(addNewWindow, "OVERLAY", "GameFontNormal")
 	title:SetPoint("TOP", 0, -4)
 	title:SetText("Add new tracked world quest")
 	-- Make it easy to access:
-	addTrackerDialog.Title = title
+	addNewWindow.Title = title
 
-	MarkWindowForMovement(addTrackerDialog)
+	MarkWindowForMovement(addNewWindow)
 
 	-- Input fields
-	local achievInput = CreateFrame("EditBox", "achievInput", addTrackerDialog, "ChatFrameEditBoxTemplate")
+	local achievInput = CreateFrame("EditBox", "achievInput", addNewWindow, "ChatFrameEditBoxTemplate")
 	achievInput:SetFrameStrata("DIALOG")
-	achievInput:SetPoint("LEFT", addTrackerDialog, 20, 0)
-	achievInput:SetPoint("RIGHT", addTrackerDialog, -20, 0)
+	achievInput:SetPoint("LEFT", addNewWindow, 20, 0)
+	achievInput:SetPoint("RIGHT", addNewWindow, -20, 0)
 	achievInput:SetHeight(24)
 	achievInput:SetMultiLine(false)
 	achievInput:SetAutoFocus(true)
-	achievInput:SetScript("OnEscapePressed", function() addTrackerDialog:Hide() end)
+	achievInput:SetScript("OnEscapePressed", function() addNewWindow:Hide() end)
 
 	-- Input field labels
-	local text = addTrackerDialog:CreateFontString(addTrackerDialog, "OVERLAY", "GameTooltipText")
+	local text = addNewWindow:CreateFontString(addNewWindow, "OVERLAY", "GameTooltipText")
 	text:SetPoint("LEFT", achievInput, 0, 10)
 	text:SetText("Achievement name:")
 
 	-- Close button
-	addTrackerDialog.CloseButton = CreateFrame("Button", nil, addTrackerDialog, "UIPanelCloseButton")
-	addTrackerDialog.CloseButton:SetPoint("TOPRIGHT", addTrackerDialog, "TOPRIGHT", 4, 3);
-	addTrackerDialog.CloseButton:SetScript("OnClick", OnCloseButtonPressed);
+	addNewWindow.CloseButton = CreateFrame("Button", nil, addNewWindow, "UIPanelButtonTemplate")
+	addNewWindow.CloseButton:SetPoint("BOTTOMLEFT", addNewWindow, "BOTTOM", 10, 10);
+	addNewWindow.CloseButton:SetPoint("TOPRIGHT", addNewWindow, "BOTTOMRIGHT", -10, 30);
+	addNewWindow.CloseButton:SetScript("OnClick", OnCloseButtonPressed);
+	addNewWindow.CloseButton:SetText("Close")
 	
-	-- Add-new button
-	local AddNew = CreateFrame("Button", "AddNew", dialogFrame, "UIPanelButtonTemplate")
-	AddNew:SetPoint("TOPLEFT", dialogFrame, "BOTTOMLEFT", 10, 30)
-	AddNew:SetPoint("BOTTOMRIGHT", dialogFrame, "BOTTOMRIGHT", -10, 10)
-	AddNew:SetText("Add")
-	AddNew:RegisterForClicks("AnyUp")
-	AddNew:SetScript("OnClick", OpenAddWindow)
+	-- Dialog add button
+	addNewWindowAddButton:SetPoint("TOPLEFT", addNewWindow, "BOTTOMLEFT", 10, 30)
+	addNewWindowAddButton:SetPoint("BOTTOMRIGHT", addNewWindow, "BOTTOM", -10, 10)
+	addNewWindowAddButton:SetText("Add")
+	addNewWindowAddButton:RegisterForClicks("AnyUp")
+	addNewWindowAddButton:SetScript("OnClick", OpenAddWindow)
 end
 
 local function Close()
-    dialogFrame:Hide()
+    mainWindow:Hide()
 end
 
 local function Open()
-    dialogFrame:Show()
+    mainWindow:Show()
 end
 
 -----------------------------------------
@@ -147,7 +179,7 @@ end
 -----------------------------------------
 
 function WQAA:SlashHandler()
-    if dialogFrame:IsShown() then
+    if mainWindow:IsShown() then
         Close()
     else
 		Open()
